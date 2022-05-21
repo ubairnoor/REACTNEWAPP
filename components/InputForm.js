e87ceec675196e893653bcb397eb03555eedaacb
ubair <ubairnoor5@gmail.com>
2022-05-21 20:16:35 +0530

    Get Values from Input Field using State and on press get input state

--- components/InputForm.js
+++ components/InputForm.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import {View,Text,TextInput} from  'react-native'
+import {View,Text,TextInput,Button} from  'react-native'
 
 
 class InputForm extends React.Component{
+    //TO Get Value from Input Field we have to Use State
+    constructor(){
+        super(); 
+        this.state={
+            name:"hello"
+        }
+    }
     render(){
         return (
             <View>
-                <Text style={{fontSize: 10}}>
-                    input Form 
+                <Text style={{fontSize: 30}}>
+                    {this.state.name}
 
                 </Text>
-                <TextInput placeholder={"Enter Your Name"}></TextInput>
+                <TextInput placeholder={"Enter Your Name"} 
+                onChangeText={(e)=>{this.setState({name:e})}} ></TextInput>
+                <Button title="submit" onPress={()=>{alert(this.state.name)}}></Button>
             </View>
         )
     }
